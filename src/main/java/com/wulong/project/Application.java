f062630b2c6dd1bc7701f9932183671c94214fb0
Wulong <491925129@qq.com>
2019-01-23 10:08:18 +0800

    更新解决跨域问题

--- src/main/java/com/wulong/project/Application.java
+++ src/main/java/com/wulong/project/Application.java
@@ -27,21 +27,5 @@ public class Application {
     public SpringUtil getSpringUtil2() {
         return new SpringUtil();
     }
-
-    @Bean
-    public FilterRegistrationBean corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
-        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-        bean.setOrder(0);
-        return bean;
-    }
-
-
 }
 
